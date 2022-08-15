e7e5797b66104da55dbb0f6fe0d4ebf2b764ce0c
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-15 19:03:36 +0300

    completed lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+};
+saturdayFun(`bathe my dog`);
+
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`
+};
+mondayWork('work from home');
+
+function wrapAdjective(message='*'){
+    return function(arg = `special`){
+    return `You are ${message}${arg}${message}!`
+}
+}
+
+
+
